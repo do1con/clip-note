c132c300df93f8687970b6a2fb9a9ce871b0843b
do1con <kss7547@gmail.com>
2021-02-24 21:40:04 +0900

    2021.02.24_kss7547 1. .env 파일 추가 - 크롬 익스텐션은 inline script를 허용하지 않음으로 .env에 INLINE_RUNTIME_CHUNK=false 옵션을 추가해줘야 함 https://stackoverflow.com/questions/63583782/react-cra-with-csp-refused-to-execute-inline-script 2. react bootstrap 추가 3. app.tsx 오타 수정

--- background.js
+++ background.js
@@ -1,25 +1,21 @@
 /* eslint-disable no-undef */
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is green.");
-  });
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: { hostEquals: 'developer.chrome.com' },
+					}),
+				],
+				actions: [new chrome.declarativeContent.ShowPageAction()],
+			},
+		]);
+	});
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "developer.chrome.com" },
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
-
-  chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("popup.html"),
-    });
-  });
+	chrome.browserAction.onClicked.addListener(function (tab) {
+		chrome.tabs.create({
+			url: chrome.runtime.getURL('build/index.html'),
+		});
+	});
 });
