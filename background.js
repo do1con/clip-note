35419d4231a14d4aa661dbb922af7d5968713052
do1con <kss7547@gmail.com>
2021-03-08 21:44:41 +0900

    2021.03.08_kss7547 1. manifest.json 수정 - 아이콘 클릭시 new tab을 띄울 수 있도록 권한 설정 2. background.js 수정 - 아이콘 클릭시 new tab을 띄우는 코드만 남김

--- background.js
+++ background.js
@@ -1,21 +1,7 @@
 /* eslint-disable no-undef */
-chrome.runtime.onInstalled.addListener(function () {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-		chrome.declarativeContent.onPageChanged.addRules([
-			{
-				conditions: [
-					new chrome.declarativeContent.PageStateMatcher({
-						pageUrl: { hostEquals: 'developer.chrome.com' },
-					}),
-				],
-				actions: [new chrome.declarativeContent.ShowPageAction()],
-			},
-		]);
-	});
-
-	chrome.browserAction.onClicked.addListener(function (tab) {
-		chrome.tabs.create({
-			url: chrome.runtime.getURL('build/index.html'),
-		});
+chrome.browserAction.onClicked.addListener(function (tab) {
+	chrome.tabs.create({
+		url: chrome.extension.getURL('build/index.html'),
+		selected: true,
 	});
 });
