106027c7d780b5b3cf37a0d9c4e051799de8ddff
do1con <kss7547@gmail.com>
2021-03-08 20:51:49 +0900

    2021.03.08_kss7547 1. eslint 룰 추가 2. 아이콘, 파비콘등 변경 3. styled-components 추가

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import styled from 'styled-components';
+import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
 
 function Header(): JSX.Element {
 	return (
 		<Container fluid>
 			<Row>
 				<Col>
-					<h1>Clip Note</h1>
+					<H1>C</H1>
+					<Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+						<Tab eventKey="home" title="Home"></Tab>
+						<Tab eventKey="profile" title="Profile"></Tab>
+						<Tab eventKey="contact" title="Contact"></Tab>
+					</Tabs>
 				</Col>
 			</Row>
 		</Container>
 	);
 }
+
+const H1 = styled.h1`
+	background: linear-gradient(45deg, rgba(141, 197, 66, 1) 0%, rgba(195, 140, 206, 1) 87%);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	color: transparent;
+	font-size: 540px;
+	display: inline;
+`;
 
 export default Header;
