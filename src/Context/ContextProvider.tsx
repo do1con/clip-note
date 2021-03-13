86eac37382e888b5fd70e77fe6958c9c712a13d2
do1con <kss7547@gmail.com>
2021-03-13 21:01:21 +0900

    2021.03.13_kss7547 1. ContextProvider.tsx 작성 (Context API)

---
+++ src/Context/ContextProvider.tsx
@@ -0,0 +1,57 @@
+import React, { useReducer } from 'react';
+
+type ClipNoteStates = Array<{
+  index: number;
+  content: string;
+}>;
+
+const initialState: ClipNoteStates = [
+  {
+    index: 1,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+  {
+    index: 2,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+  {
+    index: 3,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+  {
+    index: 4,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+  {
+    index: 5,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+  {
+    index: 6,
+    content:
+      '테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.테스트용 문서입니다.',
+  },
+];
+
+// eslint-disable-next-line import/prefer-default-export
+export const Context = React.createContext(initialState);
+
+const reducer = (state = initialState, action: any): ClipNoteStates => {
+  switch (action.type) {
+    case 'ADD/MEMO':
+      state.unshift(action.value);
+      return state;
+    default:
+      throw new Error();
+  }
+};
+
+export default function ContextProvider({ children }: { children: React.ReactNode }): JSX.Element {
+  const [state, contextDispatch] = useReducer(reducer, initialState);
+  return <Context.Provider value={initialState}>{children}</Context.Provider>;
+}
